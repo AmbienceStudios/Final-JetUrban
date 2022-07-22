128912a72debdf41bf1b3e364992e9c834bb835f
Freequency38 <Stanton.Deron@icloud.com>
2022-07-21 21:32:36 -0700

    Part 2

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,9 +1,129 @@
 import React from 'react'
 import './Artwork.css'
+import Boxer1 from '../assets/Boxer1.jpg';
+import Boxer2 from "../assets/Boxer2.jpg";
+import Boxer3 from "../assets/Boxer3.jpg";
+import Boxer4 from "../assets/Boxer4.jpg";
+import Boxer5 from "../assets/Boxer5.jpg";
+import Boxer6 from "../assets/Boxer6.jpg";
+import Boxer7 from "../assets/Boxer7.jpg";
+import Boxer8 from "../assets/Boxer8.jpg";
+import Boxer9 from "../assets/Boxer9.jpg";
+import Boxer10 from "../assets/Boxer10.jpg";
+import Boxer11 from "../assets/Boxer11.jpg";
+import Boxer12 from "../assets/Boxer12.jpg";
 
 const Artwork = () => {
   return (
-    <div>Artwork</div>
+  
+
+    <div className="slider">
+
+    <div className="slide-track">
+
+          <div className="slide">
+              <img src={Boxer1} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer2} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer3} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer4} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer5} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer6} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer7} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer8} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer9} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer10} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer11} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer12} alt="GoldenBoyNFT"/>
+          </div>
+
+
+      {/* Same Slide doubled  */}
+
+          
+      <div className="slide">
+              <img src={Boxer1} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer2} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer3} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer4} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer5} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer6} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer7} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer8} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer9} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer10} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer11} alt="GoldenBoyNFT"/>
+          </div>
+
+          <div className="slide">
+              <img src={Boxer12} alt="GoldenBoyNFT"/>
+          </div>
+
+    </div>
+
+</div>
   )
 }
 
