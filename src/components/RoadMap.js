bb2d7ab34550b76400e59cca45f0afa1bae0b498
Freequency38 <Stanton.Deron@icloud.com>
2022-07-21 20:01:00 -0700

    Road Map UI

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -3,7 +3,159 @@ import './RoadMap.css'
 
 const RoadMap = () => {
   return (
-    <div>RoadMap</div>
+    <div id="s-roadmap" className="container">
+
+    <div className="cards-road">
+        <div>
+            <h1> ROAD MAP</h1>      
+            <h3> The journey to the Urban Warrior's Stadium <br/>
+            in the Metaverse </h3>        
+        </div>
+
+  
+
+    <div className="card-container">
+          <ul className="cards-road">
+
+
+                <li className="card">
+                  <div>
+                  <h2>PHASE 1</h2>
+                    <div className="card-content">
+                    <p>Launch Urban Warriors Discord. <br/>  <br/>
+              Launch Urban Warriors GOLDEN BOY COLLECTION. </p>
+                    </div>
+                  </div>
+              
+                </li>
+    
+
+    
+                <li className="card">
+                  <div>
+                  <h2 >Pre-PHASE 2 <br/> MILESTONE REQUIREMENTS </h2>
+                    <div >
+                    <p >  75 GIVEAWAYS. <br/>  <br/> 
+              75 WHITELIST SPOTS ARE GONE. <br/> </p>  <br/> 
+                    <p>3K FOLLOWERS ON TWITTER.  </p> <br/>
+              <a href="https://twitter.com/urbanwarriornft?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="true">Follow @urbanwarriornft</a>  
+                  </div>
+                  </div>
+            
+                </li>
+
+
+                <li className="card">
+                  <div>
+                  <h2 >Phase 2</h2>
+                    <div>
+                    <p>
+             <div>
+             <p className='p-header'><strong> Launch <br/> <p className='p-header2'> WRECKLESS HANDS (2ND GEN) </p>
+             </strong></p>
+             </div> 
+              IN GAME COMPATIBLE.<br/> 
+              AUGMENTED REALITY DIGITAL CANVASES. <br/>
+              AUGMENTED REALITY SOCIAL MEDIA FILTERS.<br/> 
+              DIY SCREEN PRINTING TUTORIALS. <br/>
+              MERCHANDISE.<br/> 
+              <div >
+
+              <div>
+              <p className='p-header'><strong> Launch <br/> <p className='p-header2'>JET URBAN CLOTHING WEARABLES</p> </strong></p> 
+             </div> 
+     
+              IN GAME COMPATIBLE. <br/> 
+              AR INTERACTIVE EXPERIENCE. <br/> 
+              MERCHANDISE.
+              </div>
+              </p>
+                    </div>
+                  </div>
+
+                </li>
+
+          
+    
+                <li className="card">
+                  <div>
+                  <h2>Phase 3</h2>
+                    <div>
+                    <div>
+              <p className='p-header'><strong> Launch <br/> <p className='p-header2' >AUGMENTED REALITY APP</p> </strong></p> 
+             </div> 
+                    </div>
+                  </div>
+          
+                </li>
+   
+
+     
+                <li className="card">
+                  <div>
+                  <h2 className="notice5">Phase 4</h2>
+                    <div className="card-content">
+                  
+                    <p className='p-header'>Launch <br/> <p className='p-header2'>Urban Warriors Native Token </p> </p><br/>  <br/>
+                    <p className='p-header'> Launch <br/> <p className='p-header2'>Rewards Smart contract</p> </p> <br/> <br/>
+                    <p className='p-header'> Launch <br/> <p className='p-header2'>Breeding smart contract <br/> </p> </p>
+           
+                  </div>
+                  </div>
+          
+            
+      
+                </li>
+
+
+
+
+                <li className="card">
+                  <div>
+                  <h2 >Phase 5</h2>
+                    <div >
+                    <p className='p-header'>Launch <br/> <p className='p-header2' > Digital Real Estate Property</p>  </p><br/><br/>
+             <p> IN GAME COMPATIBLE <br/> <br/>
+              RENT OR LEASE  DIGITAL REAL ESTATES</p> 
+                    </div>
+                  </div>
+           
+                </li>
+       
+         
+
+         
+             <li className="card">
+                  <div>
+                  <h2 className="notice5">Phase 6</h2>
+                    <div className="card-content">
+                      <p className='p-header'>Launch </p> <p> into P2E GAMING  METAVERSE  🚀 </p>
+                    </div>
+                  </div>
+             
+                </li>
+       
+       
+          </ul>  
+
+    </div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div> 
+
+
+ </div> 
   )
 }
 
